d154722d9ddfdac46efc3426887ff0599e00f241
gmt <gianmaria.tarsi@gmail.com>
2019-12-07 17:38:49 +0100

    Roll Dice btn

--- app.js
+++ app.js
@@ -8,3 +8,47 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer, dice;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+// Hide dice 
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
+
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    // Random Number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    // Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'asset/dice-' + dice + '.png';
+    // Update score but IF the rolled number was NOT a 1
+    if (dice !== 1) {
+        // Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        // Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        diceDOM.style.display = 'none';
+
+    }
+
+});
