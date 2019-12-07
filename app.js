c2f38068d45444d902ec6f4b33a13f104f737df1
gmt <gianmaria.tarsi@gmail.com>
2019-12-07 18:47:43 +0100

    Add new game btn
    Init function

--- app.js
+++ app.js
@@ -12,17 +12,9 @@ GAME RULES:
 var scores, roundScore, activePlayer, dice;
 var diceDOM = document.querySelector('.dice');
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+init();
 
-// Hide dice 
-document.querySelector('.dice').style.display = 'none';
 
-document.getElementById('score-0').textContent = 0;
-document.getElementById('score-1').textContent = 0;
-document.getElementById('current-0').textContent = 0;
-document.getElementById('current-1').textContent = 0;
 
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
@@ -61,6 +53,32 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     }
 });
 
+document.querySelector('.btn-new').addEventListener('click', init);
+
+// Functions
+function init() {
+
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+
+    // Hide dice 
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = 0;
+    document.getElementById('score-1').textContent = 0;
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+    document.querySelector('#name-0').textContent = 'Player 1';
+    document.querySelector('#name-1').textContent = 'Player 2';  
+    document.querySelector('.player-0-panel').classList.remove('winner'); 
+    document.querySelector('.player-1-panel').classList.remove('winner'); 
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active'); 
+    
+    document.querySelector('.player-0-panel').classList.add('active');
+  };
+
 function nextPlayer() {  
     // Next player
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
@@ -71,6 +89,6 @@ function nextPlayer() {
 
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
-    
+
     diceDOM.style.display = 'none';
  }
